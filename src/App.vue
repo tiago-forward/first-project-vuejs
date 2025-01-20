3055f9581d987c7632ab48f7d189103112de16e9
tiagoforward <devtiagoforward@gmail.com>
2025-01-20 11:33:26 -0300

    initializing project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <h1>Minha playlist de músicas</h1>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+  margin-top: 4rem;
+}
+@media (min-width: 1024px) {
+
+}
+</style>
