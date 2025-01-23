e64c76e56ec9f91927c087572ddbd50701f8b8f1
tiagoforward <devtiagoforward@gmail.com>
2025-01-23 09:34:27 -0300

    inserted new component layouts

---
+++ src/layouts/LayoutMain.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LayoutMain',
+  setup() { }
+})
+</script>
+
+<template>
+  <header>
+    <slot name="searchBar"></slot>
+  </header>
+
+  <main>
+    <slot></slot>
+  </main>
+
+  <footer :class="classes.footer">
+    <slot name="footer"></slot>
+  </footer>
+</template>
+
+<style module="classes">
+.footer {
+  text-align: center;
+  margin-bottom: 2rem;
+  margin-top: 2rem;
+}
+</style>
